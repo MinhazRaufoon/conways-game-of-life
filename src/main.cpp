6ae885d19262574ed79371f58b1b899cd26fbd31
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-06-12 01:32:14 +0200

    Add evolution function

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 int main()
 {
   Region reg;
-  reg[10][10] = true;
-  reg[10][11] = true;
-  reg[10][12] = true;
+  reg(10, 10) = true;
+  reg(10, 11) = true;
+  reg(11, 11) = true;
+  reg(9, 11) = true;
+  reg(9, 12) = true;
   std::cout << reg << std::endl;
+
+  std::cout << std::endl;
+
+  std::cout << reg.evolve() << std::endl;
+
   return 0;
 }
--- src/region.cpp
+++ src/region.cpp
@@ -15,3 +15,70 @@ std::ostream &operator<<(std::ostream &out, const Region &region)
   }
   return out;
 }
+
+bool &Region ::operator()(int row, int col)
+{
+  if (row < 0)
+    return this->neighborEdges["top"][col];
+  else if (row >= this->LENGTH)
+    return this->neighborEdges["bottom"][col];
+  else if (col < 0)
+    return this->neighborEdges["left"][row];
+  else if (col >= this->LENGTH)
+    return this->neighborEdges["right"][row];
+  else
+    return this->grid[row][col];
+}
+
+bool &Region::cell(int row, int col)
+{
+  return this->operator()(row, col);
+}
+
+int Region::countAliveNeighbors(int row, int col)
+{
+  int count{0};
+  for (int i{row - 1}; i <= row + 1; i++)
+  {
+    for (int j{col - 1}; j <= col + 1; j++)
+    {
+      if (i == row && j == col)
+        continue;
+      if (cell(i, j))
+        count++;
+    }
+  }
+  return count;
+}
+
+int Region::countDeadNeighbors(int row, int col)
+{
+  return 8 - this->countAliveNeighbors(row, col);
+}
+
+Region Region::evolve()
+{
+  Region evolved;
+
+  // For all cells
+  for (int r{}; r < this->LENGTH; r++)
+  {
+    for (int c{}; c < this->LENGTH; c++)
+    {
+      int aliveNeighborCount = this->countAliveNeighbors(r, c);
+
+      if (this->cell(r, c))
+      {
+        // For alive cell
+        evolved.cell(r, c) = aliveNeighborCount == 2 || aliveNeighborCount == 3;
+      }
+      else
+      {
+        // For dead cell
+        evolved.cell(r, c) = aliveNeighborCount == 3;
+      }
+    }
+  }
+
+  return evolved;
+}
