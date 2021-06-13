2a275fa17b094e6cb04f6610814f59298232499b
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-06-13 17:05:42 +0200

    Add header for simpler handling of game

--- game-lib/src/game_of_life.cpp
+++ game-lib/src/game_of_life.cpp
@@ -2,10 +2,21 @@
 #include "universe.h"
 #include "region.h"
 
+Universe *g_conwayUniverse{nullptr};
+
 int GameOfLife_Init(
     std::function<void(int, int)> pointDrawer,
     std::function<void()> displayCleaner,
-    std::function<void()> displayUpdater) { return 0; }
+    std::function<void()> displayUpdater)
+{
+  g_conwayUniverse = new Universe;
+
+  g_conwayUniverse->setDisplayCleaner(displayCleaner);
+  g_conwayUniverse->setDisplayUpdater(displayUpdater);
+  g_conwayUniverse->setPointDrawer(pointDrawer);
+
+  return 0;
+}
 
 int GameOfLife_GetTotalHorizontalCells()
 {
@@ -17,9 +28,15 @@ int GameOfLife_GetTotalVerticalCells()
   return Universe::MAX_ROWS * Region::LENGTH;
 }
 
-int GameOfLife_Begin() { return 0; }
+int GameOfLife_Begin()
+{
+  g_conwayUniverse->begin();
+  return 0;
+}
 
 int GameOfLife_Exit()
 {
+  g_conwayUniverse->end();
+  delete g_conwayUniverse;
   return 0;
 }
